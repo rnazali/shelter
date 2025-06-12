baa99f1400084b8eb01652025b693938460635ce
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-06-12 12:46:48 +0700

    add bootstrap theme

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-bs-theme="light">
       <body>
         {children}
       </body>
