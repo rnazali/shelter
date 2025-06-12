9229e19115f2ffdadd0d23b7f085f0069ef928ee
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-06-12 12:53:29 +0700

    add getLorem

---
+++ components/utilities.js
@@ -0,0 +1,14 @@
+export {
+  getLorem
+}
+
+function getLorem() {
+  return (
+    <>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat orci id nisi sollicitudin aliquet. Etiam consectetur neque sit amet felis commodo, at molestie diam aliquet. Duis iaculis mauris et scelerisque convallis. Duis lectus elit, egestas at velit id, mattis rutrum urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer pharetra finibus ex at egestas. Donec luctus sit amet dolor lacinia malesuada.</p>
+      <p>In volutpat pellentesque nunc sed feugiat. Nulla quis quam sem. Nam pellentesque interdum urna in sagittis. Donec feugiat nulla iaculis sem ultrices, vel interdum purus vehicula. Phasellus id lectus lacus. Aliquam eleifend elit et mi aliquet viverra. Vivamus hendrerit volutpat est, id ornare turpis lacinia quis. Sed quis magna posuere, aliquet enim ac, cursus erat. Morbi justo felis, efficitur eu vehicula vitae, fringilla at enim. Maecenas condimentum accumsan ante id laoreet. Donec velit ex, tempor dignissim ligula quis, tempor vestibulum magna. Nulla laoreet orci ac mauris porta gravida. Vestibulum sit amet arcu tincidunt ex faucibus accumsan et vitae leo.</p>
+      <p>Etiam nunc urna, iaculis aliquet ipsum nec, dictum mollis turpis. Phasellus maximus interdum lorem ut ullamcorper. Maecenas et nibh sed augue dapibus condimentum. Sed luctus id nisi quis tempus. Integer a sapien ac felis eleifend semper. Vestibulum varius quis risus non congue. Donec eget finibus lacus. Pellentesque ultricies pellentesque ipsum ac molestie. Sed suscipit, metus eleifend commodo tempor, nibh neque condimentum felis, vel faucibus dolor neque porta lorem. Phasellus velit est, pellentesque at eleifend vehicula, bibendum eget lectus. Etiam dignissim tortor ac porttitor accumsan. Nam lacinia eleifend blandit. Mauris vel tortor congue, ultricies orci vel, vestibulum diam.</p>
+      <p>Quisque finibus maximus vulputate. In sed venenatis ante, non eleifend mauris. Duis laoreet finibus sem id sodales. Proin feugiat lectus sed risus commodo venenatis. Sed ornare metus vehicula eros mattis posuere in ac orci. Nulla euismod mollis odio ut aliquam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus pretium nunc dolor, et rutrum quam fermentum ut. Sed condimentum mauris malesuada porta sagittis. Nunc tempus in magna eget eleifend. Ut at interdum est, eu rhoncus odio.</p>
+    </>
+  )
+}
