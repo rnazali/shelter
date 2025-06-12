837cedcdf9a6a56ff5bd7e13b88148ff0f081569
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-06-12 15:03:51 +0700

    configure mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
+import createMDX from '@next/mdx'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   output: "export",
   images: {
     unoptimized: true
   }
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add Markdown plugins here
+})
+
+export default withMDX(nextConfig);
