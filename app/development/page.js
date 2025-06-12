9c077ed46ffa6c05c522e8aeaa42c1356e4f9933
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2025-06-12 13:03:37 +0700

    update development page

--- app/development/page.js
+++ app/development/page.js
@@ -1,5 +1,22 @@
+import {Alert, Button, Stack} from "react-bootstrap";
+import {getLorem} from "@/components/utilities";
+
+
 export default function Development() {
   return (
-    <p>Development page</p>
+    <div>
+      <h1>Development page</h1>
+
+      {getLorem()}
+
+      <Stack direction="horizontal" gap={1} className={"mb-4"}>
+        <Button variant={"primary"}>Asd</Button>
+        <Button variant="danger">Asd</Button>
+        <Button variant="outline-danger">Asd</Button>
+      </Stack>
+
+      <Alert variant="warning">Alert</Alert>
+
+    </div>
   )
 }
